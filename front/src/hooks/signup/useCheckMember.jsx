d35ebfb63bc981eb5b8a7140738b5ecdf352d3a9
Woohyeok Baek <qordngur156@gmail.com>
2023-01-16 13:21:09 +0900

    로그인 리팩토링

--- front/src/hooks/signup/useCheckMember.jsx
+++ front/src/hooks/signup/useCheckMember.jsx
@@ -20,7 +20,7 @@ export default function useCheckMember() {
         const { data } = await axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${accessToken}`)
         const response = await axios.post('http://localhost:3001/user/test', { data : data.id })
 
-        accessToken && !response.data.data ? window.location.replace(): window.location.replace('/list')
+        accessToken && !response.data.data ? window.location.replace('/signup'): window.location.replace('/list')
         dispatch( setSignUpUserData(data) ) // 비회원일 경우, signup 컴포넌트에서 데이터를 사용할수 있게, state변경함!
         setRenderState(true) // 위 동작이 다끝나고 조건부로 main 컴포넌트를 로드함!
       }
